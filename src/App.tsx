89e457192f9642717a6ed4a58692b8423b503898
nel <neeeel@qq.com>
2024-02-07 18:30:47 +0800

    居然必须要加分号

--- src/App.tsx
+++ src/App.tsx
@@ -118,8 +118,8 @@ function App() {
    */
   function shuffleArray(array: Color[]) {
     for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1))
-      [array[i], array[j]] = [array[j], array[i]]
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
     }
   }
 
