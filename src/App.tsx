47639481fa08db7299150f55b63edd6c63357b86
nel <neeeel@qq.com>
2024-02-08 18:06:31 +0800

    超越之力首次不显示，修改深色模式时的文字颜色

--- src/App.tsx
+++ src/App.tsx
@@ -220,7 +220,14 @@ function App() {
             {played ? '重新开始' : '开始'}
           </button>
         </div>
-        <Checkbox disabled={played ? false : true} onChange={(e) => changeTheEcho(e.target.checked)}>超越之力</Checkbox>
+        {played ?
+          <Checkbox
+            className='checkbox'
+            onChange={(e) => changeTheEcho(e.target.checked)}>
+            超越之力
+          </Checkbox>
+          : <></>
+        }
         <p className="read-the-docs">
           染剂色值来自
           <a href="https://ff14.huijiwiki.com/wiki/%E6%9F%93%E5%89%82" target="_blank" rel="noopener noreferrer">
